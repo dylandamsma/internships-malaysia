651524cb433460096fbfb091117292a9468585a5
Dylan Damsma <dylan@foundingbird.com>
2020-01-27 04:15:29 +0800

    Change header title

--- components/header/index.js
+++ components/header/index.js
@@ -36,7 +36,7 @@ const Header = props => {
                             <Logo>
                                 <Link href="/">
                                     <a>
-                                        <Heading element="div" type="title2">InternsWin</Heading>
+                                        <Heading element="div" type="title2">InternsMalaysia</Heading>
                                     </a>
                                 </Link>
                             </Logo>
