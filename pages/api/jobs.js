92cf339bdff51af795d7d3e9bebb4988a6acbbf4
Dylan Damsma <dylan@foundingbird.com>
2020-01-27 22:40:18 +0800

    Change API code

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -4,12 +4,12 @@ const { serialize } = require('../../helpers/utilities')
 module.exports = (req, res) => {
 	Promise.resolve(getJobPosts())
 		.then(data => {
-			res.status(200, { 'Content-Type': 'application/json' })
+			res.writeHead(200, { 'Content-Type': 'application/json' })
 			return res.end(serialize(data))
 		})
 		.catch(error => {
 			console.log(error)
-			res.status(200, { 'Content-Type': 'application/json' })
+			res.writeHead(200, { 'Content-Type': 'application/json' })
 			return res.end(serialize({}))
 		})
 }
