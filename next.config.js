35d7d05f1479d4c0d533f5293877cc1dd8c09297
Dylan Damsma <dylandamsma@gmail.com>
2020-01-29 00:00:11 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-      airtableKey: 'keypgQQAJbjW63VAl',
-      airtableBase: 'appCybbMKxZZay5iw',
-      airtablePostsTable: 'posts',
-      SITE_URL: 'https://jobsite.now.sh',
-    },
-  }
